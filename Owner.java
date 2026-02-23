c1ad31d8fff61529763b12c607cc66407c1ffd2b
MEHSOYY <72802928+MEHSOYY@users.noreply.github.com>
2026-02-22 23:33:56 -0500

    Update Owner.java
    changed it so it syncs with jframe

--- Owner.java
+++ Owner.java
@@ -1,22 +1,39 @@
-public class Owner extends User{
-	
-	private String vehicleID;
-	private String vehicleModel;
-	private String vehicleMake;
-	private int vehicleYear;
-	private String residencyTime;
-	
-	public Owner(String ownerID, String vehicleID, String vehicleModel, String vehicleMake, int vehicleYear, String residencyTime) {
-		super(ownerID);
-		this.vehicleID = vehicleID;
-		this.vehicleModel = vehicleModel;
-		this.vehicleMake = vehicleMake;
-		this.vehicleYear = vehicleYear;
-		this.residencyTime = residencyTime;
-	}
-	@Override
-	public String fileText() {
-		return "Owner ID: " + ID + " | Timestamp: " + time + "| Vehicle ID" + vehicleID +  "| Vehicle model: " + vehicleModel + " | Vehicle make: " + vehicleMake +
-				" | Vehicle year: " + vehicleYear + " | Estimated residency time: " + residencyTime;
-	}
+public class Owner extends User {
+    private String vehicleID;
+    private String vehicleModel;
+    private String vehicleMake;
+    private int vehicleYear;
+    private String arrivalTime;
+    private String departureTime;
+
+    public Owner(String ownerID, String vehicleID, String vehicleModel, String vehicleMake,
+                 int vehicleYear, String arrivalTime, String departureTime) {
+        super(ownerID);
+        this.vehicleID = vehicleID;
+        this.vehicleModel = vehicleModel;
+        this.vehicleMake = vehicleMake;
+        this.vehicleYear = vehicleYear;
+        this.arrivalTime = arrivalTime;
+        this.departureTime = departureTime;
+    }
+
+    public String getVehicleID()    { return vehicleID; }
+    public String getVehicleModel() { return vehicleModel; }
+    public String getVehicleMake()  { return vehicleMake; }
+    public int getVehicleYear()     { return vehicleYear; }
+    public String getArrivalTime()  { return arrivalTime; }
+    public String getDepartureTime(){ return departureTime; }
+
+   
+    public String fileText() {
+        return "Owner ID: " + ID +
+               " | Timestamp: " + time +
+               " | Vehicle ID: " + vehicleID +
+               " | Vehicle Model: " + vehicleModel +
+               " | Vehicle Make: " + vehicleMake +
+               " | Vehicle Year: " + vehicleYear +
+               " | Arrival Time: " + arrivalTime +
+               " | Departure Time: " + departureTime;
+    }
 }
+
